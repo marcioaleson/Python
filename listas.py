efc9c49fd66a0858aa296c1d6d856050aeca821e
marcioaleson <marcioaleson@gmail.com>
2017-03-11 23:29:53 -0300

    Loop com listas.

--- listas.py
+++ listas.py
@@ -3,3 +3,18 @@ if lista:
     print("Nunca sou executado")
 else:
     print("Sempre sou executado")
+
+impostos = ['MEI', 'Simples']
+for imposto in impostos:
+    print(imposto)
+
+
+impostos = ['MEI', 'Simples']
+for imposto in impostos:
+	if imposto.startswith("S"):
+	    continue
+	print(imposto)
+
+lista = [0,1,2,3,4,5,6,7,8,9]
+for i in lista:
+	print(i)
