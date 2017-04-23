38ce89d9efde0046d3f341dfdbf7587070c4f756
marcioaleson <marcioaleson@gmail.com>
2017-04-23 12:13:20 -0300

    Testes com unittest em Python.

--- Testes/lista_dados.py
+++ Testes/lista_dados.py
@@ -28,4 +28,17 @@ class ColumnTest(unittest.TestCase):
     def test_vaildate_bigint(self):
         self.assertTrue(Column.validate('bigint',100))
         self.assertTrue(not Column.validate('bigint',10.1))
-        self.
+        self.assertTrue(not Column.validate('bigint','Texto'))
+
+    def test_validate_varchar(self):
+        self.assertTrue(Column.validate('varchar','Texto'))
+        self.assertTrue(not Column.validate('varchar',100))
+        self.assertTrue(not Column.validate('varchar', 10.1))
+
+    def test_validate_numeric(self):
+        self.assertTrue(Column.validate('numeric',10.1))
+        self.assertTrue(not Column.validate('varchar', 100))
+        self.assertTrue(not Column.validate('varchar', 10.1))
+
+if __name__ == "__main__":
+    unittest.main()
