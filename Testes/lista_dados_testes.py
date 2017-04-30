d200e269739d17cbeeb705b6e658d70734820ca0
marcioaleson <marcioaleson@gmail.com>
2017-04-29 21:51:48 -0300

    Testes em python com o unittest.'

---
+++ Testes/lista_dados_testes.py
@@ -0,0 +1,19 @@
+# encoding: utf-8
+
+from domein import Column
+
+class ColumnTest(unittest.TestCase):
+    def test_validate_bigint(self):
+        self.assertTrue(Column.validate('bigint', 100))
+        self.assertTrue( not Column.validate('bigint', 10.1))
+        self.assertTrue( not Column.validate('bigint', 'teste'))
+
+    def test_validate_numeric(self):
+        self.assertTrue(Column.validate('numeric', 10.1))
+        self.assertTrue(Column.validate('numeric', 100))
+        self.assertTrue( not Column.validate('numeric', 'teste'))
+
+    def test_validate_varchar(self):
+        self.assertTrue(Column.validate('varchar', 'teste'))
+        self.assertTrue( not Column.validate('varchar', 100))
+        self.assertTrue( not Column.validate('varchar', 10.1))
