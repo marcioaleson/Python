336b8431229c81f41332067115f03aebb5eaeb4b
marcioaleson <marcioaleson@gmail.com>
2017-05-06 21:46:53 -0300

    Custom tests with Python. "

--- Testes/lista_dados_com_testes_customizados.py
+++ Testes/lista_dados_com_testes_customizados.py
@@ -14,3 +14,36 @@ HTML = '''\
 
 OK_TD = '<tr><td style="color: green;">{}</td></tr>'
 ERR_TD = '<tr><td style="color: red;">{}</td></tr>'
+
+class HTMLTestResult(unittest.TestResult):
+    def __init__(self, runner):
+        unittest.TestResult.__init__(self)
+        self.runner = runner
+        self.infos = []
+        self.current = {}
+
+    def newTest(self):
+        self.infos.append(self.current)
+        self.current = {}
+
+    def startTest(self, test):
+        self.current['id'] = test.id()
+
+    def addSuccess(self, test):
+        self.current['result'] = 'ok'
+        self.newTest()
+
+    def addError(self, test, err):
+        self.current['result'] = 'error'
+        self.newTest()
+
+    def addFailure(self, test, err):
+        self.current['result'] = 'fail'
+        self.newTest()
+
+    def addSkip(self, test, err):
+        self.current['result'] = 'skipped'
+        self.current['reason'] = err
+        self.newTest()
+
+    
